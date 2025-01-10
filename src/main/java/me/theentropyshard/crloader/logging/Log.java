43166fe7ccd4088af4c0a7905482aac55aeca924
TheEntropyShard <theentropyshard@gmail.com>
2025-01-10 23:27:29 +0300

    Updated copyright year

--- src/main/java/me/theentropyshard/crloader/logging/Log.java
+++ src/main/java/me/theentropyshard/crloader/logging/Log.java
@@ -1,6 +1,6 @@
 /*
  * CRLoader - https://github.com/CRLauncher/CRLoader
- * Copyright (C) 2024 CRLauncher
+ * Copyright (C) 2024-2025 CRLauncher
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
