210a85e8386d8cf53cef74e384549b115626f356
TheEntropyShard <theentropyshard@gmail.com>
2025-03-13 15:34:36 +0300

    Corrected PR #1

--- src/main/java/me/theentropyshard/crloader/patch/JoinServerPatch.java
+++ src/main/java/me/theentropyshard/crloader/patch/JoinServerPatch.java
@@ -1,29 +1,51 @@
+/*
+ * CRLoader - https://github.com/CRLauncher/CRLoader
+ * Copyright (C) 2024-2025 CRLauncher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package me.theentropyshard.crloader.patch;
+
 import javassist.*;
 import me.theentropyshard.crloader.ClassName;
 
 public class JoinServerPatch extends Patch {
-    private static final ClassName MAIN_MENU = new ClassName("finalforeach", "cosmicreach", "gamestates", "MainMenu");
+    private static final ClassName MAIN_MENU = new ClassName("finalforeach", "cosmicreach", "ClientSingletons");
+
     private final String serverAddress;
-    public JoinWorldServerPatch() {
-        super("Join Server Patch", JoinWorldServerPatch.MAIN_MENU);
+
+    public JoinServerPatch() {
+        super("Join Server Patch", JoinServerPatch.MAIN_MENU);
+
         this.serverAddress= System.getProperty("crloader.joinServerAddress");
     }
     @Override
     public boolean isActive() {
         return this.serverAddress!= null && !this.serverAddress.trim().isEmpty();
     }
+
     @Override
     public byte[] perform(CtClass clazz) throws Exception {
         CtMethod method = clazz.getDeclaredMethod("create");
-        String injectedCode = "{\n" +
-                "\tfinalforeach.cosmicreach.gamestates.GameState.switchToGameState(" +
-                "new finalforeach.cosmicreach.gamestates.ConnectingScreen(\"" + this.serverAddress+ "\"));\n" +
-                "};\n";
-        System.out.println(injectedCode);
-        method.insertAfter(injectedCode);
+
+        method.insertAfter("{ finalforeach.cosmicreach.gamestates.GameState.currentGameState = new finalforeach.cosmicreach.gamestates.ConnectingScreen(\"" + this.serverAddress + "\"); }");
+
         byte[] bytecode = clazz.toBytecode();
+
         clazz.detach();
+
         return bytecode;
     }
 }
